f26c2f755de948d26cb2f7fdad2c7cc3bdb10abd
Kaif Ahmed <dm@kaif.dev>
2021-03-16 11:38:03 +0000

    migrated from vim-plug to packer

---
+++ files/dotfiles/nvim/.config/nvim/init.lua
@@ -0,0 +1,12 @@
+--  _       _ _     _             
+-- (_)_ __ (_) |_  | |_   _  __ _ 
+-- | | '_ \| | __| | | | | |/ _` |
+-- | | | | | | |_ _| | |_| | (_| |
+-- |_|_| |_|_|\__(_)_|\__,_|\__,_|
+                               
+require('plugins')
+require('plugins/compe-config')
+
+require('binds')
+require('options')
+require('lsp')
---
+++ files/dotfiles/nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,28 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+    -- lsp
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-compe'
+    use 'anott03/nvim-lspinstall'
+
+    -- helpers
+    use 'tpope/vim-vinegar'
+    use 'millermedeiros/vim-statline'
+    use 'rhlobo/vim-super-retab'
+    use 'tpope/vim-commentary'
+
+    -- tools
+    use 'tpope/vim-fugitive'
+
+    -- linting
+    -- use 'dense-analysis/ale'
+end)
