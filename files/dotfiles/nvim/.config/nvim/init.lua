1ccd522e8efd803f82ca05a78caaea54f1765263
Kaif Ahmed <dm@kaif.dev>
2021-03-22 00:05:03 +0000

    enabled ale and compe configs

--- files/dotfiles/nvim/.config/nvim/init.lua
+++ files/dotfiles/nvim/.config/nvim/init.lua
@@ -4,10 +4,11 @@
 -- | | | | | | |_ _| | |_| | (_| |
 -- |_|_| |_|_|\__(_)_|\__,_|\__,_|
 print('DEBUG: loading configs...')
--- require('plugins')
--- require('plugins/compe-config')
 
 require('binds')
 require('options')
 require('lsp')
+require('plugins/ale-config')
+require('plugins/compe-config')
+
 print('- init.lua loaded!')
--- files/dotfiles/nvim/.config/nvim/lua/plugins/ale-config.lua
+++ files/dotfiles/nvim/.config/nvim/lua/plugins/ale-config.lua
@@ -9,3 +9,5 @@ vim.api.nvim_set_var('ale_javascript_prettier_use_local_config', 1)
 vim.api.nvim_set_var('ale_set_highlights', 0)
 vim.api.nvim_set_var('ale_disable_lsp', 1 )
 vim.api.nvim_set_var('ale_completion_enabled', 0)
+
+print('- plugins/ale-config.lua loaded!')
