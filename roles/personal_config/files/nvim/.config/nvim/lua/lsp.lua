4312f032701a196d8affa3c4c9a19db311ef9694
Kaif Ahmed <dm@kaif.dev>
2021-08-05 08:15:12 +0100

    feat: added eslint diagnostic support

--- roles/personal_config/files/nvim/.config/nvim/lua/lsp.lua
+++ roles/personal_config/files/nvim/.config/nvim/lua/lsp.lua
@@ -1,9 +1,52 @@
 local lspconfig = require('lspconfig')
+local util = require ('lspconfig').util
 
 -- language: JavaScript/TypeScript
 -- requirements: 'npm', 'typescript', 'typescript-language-server'
 -- command: npm install -g typescript typescript-language-server
 lspconfig.tsserver.setup{}
+
+lspconfig.diagnosticls.setup{
+    filetypes = {"javascript", "typescript"},
+    root_dir = function(fname)
+	return util.root_pattern("tsconfig.json")(fname) or
+	util.root_pattern(".eslintrc.js")(fname);
+    end,
+    init_options = {
+	linters = {
+	    eslint = {
+		command = "./node_modules/.bin/eslint",
+		rootPatterns = {".eslintrc.js", ".git"},
+	  	debounce = 100,
+	  	args = {
+		    "--stdin",
+	  	    "--stdin-filename",
+	  	    "%filepath",
+	  	    "--format",
+	  	    "json"
+	  	},
+		sourceName = "eslint",
+		parseJson = {
+		    errorsRoot = "[0].messages",
+	    	    line = "line",
+	    	    column = "column",
+	    	    endLine = "endLine",
+	    	    endColumn = "endColumn",
+	    	    message = "[eslint] ${message} [${ruleId}]",
+	    	    security = "severity"
+		},
+		securities = {
+		    [2] = "error",
+	    	    [1] = "warning"
+		}
+	    },
+	},
+	filetypes = {
+	    javascript = "eslint",
+	    typescript = "eslint"
+	}
+    }
+}
 
 -- language: YAML
 -- requirements: 'npm', 'yaml-language-server'
