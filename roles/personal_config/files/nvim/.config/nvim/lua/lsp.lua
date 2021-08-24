943863cf748db86dea15b55c449b0c830681989e
Kaif Ahmed <dm@kaif.dev>
2021-08-24 09:05:12 +0100

    feat: added LSP configs for HTML, CSS, JSON and Ansible

--- roles/personal_config/files/nvim/.config/nvim/lua/lsp.lua
+++ roles/personal_config/files/nvim/.config/nvim/lua/lsp.lua
@@ -1,6 +1,23 @@
 local lspconfig = require('lspconfig')
 local util = require ('lspconfig').util
 
+-- language: Ansible
+-- requirements: 'npm', 'ansible-language-server'
+-- command: npm i -g ansible-language-server
+lspconfig.ansiblels.setup{}
+
+-- language: BASH
+-- requirements: 'npm', 'bash-language-server'
+-- command: npm i -g bash-language-server
+lspconfig.bashls.setup{}
+
+-- languages: HTML/CSS/JSON
+-- requirements: 'npm', 'vscode-langservers-extracted'
+-- command: npm i -g vscode-langservers-extracted
+lspconfig.html.setup{}
+lspconfig.cssls.setup{}
+lspconfig.jsonls.setup{}
+
 -- language: JavaScript/TypeScript
 -- requirements: 'npm', 'typescript', 'typescript-language-server'
 -- command: npm install -g typescript typescript-language-server
@@ -52,11 +69,6 @@ lspconfig.diagnosticls.setup{
 -- requirements: 'npm', 'yaml-language-server'
 -- command: npm install -g yaml-language-server
 lspconfig.yamlls.setup{}
-
--- language: BASH
--- requirements: 'npm', 'bash-language-server'
--- command: npm i -g bash-language-server
-lspconfig.bashls.setup{}
 
 -- options
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
