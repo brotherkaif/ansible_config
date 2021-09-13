95345ed2328bbf541e3b9dcc69c666fd6ffd8e15
Kaif Ahmed <dm@kaif.dev>
2021-09-13 16:39:30 +0100

    fix: updated `focus.nvim` config slightly

--- roles/dotfiles/files/nvim/.config/nvim/lua/plugins.lua
+++ roles/dotfiles/files/nvim/.config/nvim/lua/plugins.lua
@@ -25,7 +25,7 @@ return require('packer').startup(function()
     use 'millermedeiros/vim-statline'
 
     -- window management
-    use { "beauwilliams/focus.nvim", config = function() require("focus").setup() end }
+    use { "beauwilliams/focus.nvim", config = function() require("focus").setup({signcolumn = false, number=false, hybridnumber = true, winhighlight = true}) end }
 
     -- completion
     use 'hrsh7th/nvim-compe'
