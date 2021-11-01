4b71ca33cc50e72c1faac61e679c7087a5b431a7
Kaif Ahmed <dm@kaif.dev>
2021-11-01 17:28:00 +0000

    fix: fixed prettier typescript issue in nvim

--- roles/dotfiles/files/nvim/.config/nvim/lua/plugins/formatter-config.lua
+++ roles/dotfiles/files/nvim/.config/nvim/lua/plugins/formatter-config.lua
@@ -11,6 +11,16 @@ require('formatter').setup({
           }
         end
     },
+    typescript = {
+        -- prettier
+       function()
+          return {
+            exe = "prettier",
+            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), '--single-quote'},
+            stdin = true
+          }
+        end
+    },
     -- lua = {
     --     -- luafmt
     --     function()
